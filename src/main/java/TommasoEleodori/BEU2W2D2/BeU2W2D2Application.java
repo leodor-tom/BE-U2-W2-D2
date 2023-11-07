35c7c13d7546f11e2802fa9e0f87f655a088458c
leodor-tom <eleodoritom@gmail.com>
2023-11-07 15:00:48 +0100

    Initial commit

---
+++ src/main/java/TommasoEleodori/BEU2W2D2/BeU2W2D2Application.java
@@ -0,0 +1,13 @@
+package TommasoEleodori.BEU2W2D2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeU2W2D2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeU2W2D2Application.class, args);
+	}
+
+}
---
+++ src/test/java/TommasoEleodori/BEU2W2D2/BeU2W2D2ApplicationTests.java
@@ -0,0 +1,13 @@
+package TommasoEleodori.BEU2W2D2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeU2W2D2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
